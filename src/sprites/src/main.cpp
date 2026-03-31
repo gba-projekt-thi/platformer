f79b33c811741b1d4409a8d1507e11962674933f
Erik <erik.s.foerster@gmail.com>
2026-03-31 15:43:51 +0000

    refactor: changed Comments to english und format

--- src/sprites/src/main.cpp
+++ src/sprites/src/main.cpp
@@ -2,7 +2,7 @@
 #include "bn_keypad.h"
 #include "bn_sprite_ptr.h"
 #include "bn_sprite_tiles_ptr.h"
-#include "bn_sprite_items_ente.h"   // verwendetes sprite
+#include "bn_sprite_items_ente.h"  // used sprite
 #include "bn_bg_palettes.h"
 
 #include "bn_sprites_mosaic.h"
@@ -36,78 +36,68 @@ int main()
 {
     bn::core::init();
 
-    // Sprite wird erstellt
+    // Create sprite
     bn::sprite_ptr ente_sprite = bn::sprite_items::ente.create_sprite(0, 0);
 
-    // hintergrund farbe
-    // grau     16,16,16
-    // blau     00,00,16
-    //      
-    bn::bg_palettes::set_transparent_color(bn::color(12,20,31)); 
-    // für animation
+    // Background color
+    // gray     16,16,16
+    // blue     00,00,16
+    bn::bg_palettes::set_transparent_color(bn::color(12, 20, 31));
+
+    // For animation
     bn::sprite_animate_action<2> action = bn::create_sprite_animate_action_forever(
-                    ente_sprite, 26, bn::sprite_items::ente.tiles_item(), 0, 1);
-    // var für bewegungs-check
-    // 0 = nichts
-    // 1 = rechts
-    // -1 = links
+        ente_sprite, 26, bn::sprite_items::ente.tiles_item(), 0, 1);
+
+    // Variable for movement check
+    // 0 = nothing
+    // 1 = right
+    // -1 = left
     int direction = 0;
     
-    while(!bn::keypad::start_pressed())
-    {
+    while (!bn::keypad::start_pressed()) {
         bool moving = false;
 
-        // Bewegung
-        if(bn::keypad::left_held())
-        {       
+        // Movement
+        if (bn::keypad::left_held()) {
             ente_sprite.set_x(ente_sprite.x() - 1);
-            if(direction != -1) 
-            {
+            if (direction != -1) {
                 action = bn::create_sprite_animate_action_forever(
-                ente_sprite, 10, bn::sprite_items::ente.tiles_item(), 4, 5);
+                    ente_sprite, 10, bn::sprite_items::ente.tiles_item(), 4, 5);
                 direction = -1;
             }
             moving = true;
         }
-        if(bn::keypad::right_held())
-        {            
+        if (bn::keypad::right_held()) {
             ente_sprite.set_x(ente_sprite.x() + 1);
-            if(direction != 1) 
-            {
+            if (direction != 1) {
                 action = bn::create_sprite_animate_action_forever(
-                ente_sprite, 10, bn::sprite_items::ente.tiles_item(), 0, 1);
+                    ente_sprite, 10, bn::sprite_items::ente.tiles_item(), 0, 1);
                 direction = 1;
             }
             moving = true;
         }
 
-        if(bn::keypad::up_held())
-        {
+        if (bn::keypad::up_held()) {
             direction = 0;
-            // nur sprite anpassen
+            // Only adjust sprite
             ente_sprite.set_tiles(bn::sprite_items::ente.tiles_item().create_tiles(9));
         }
-        if(bn::keypad::down_held())
-        {
+        if (bn::keypad::down_held()) {
             direction = 0;
-            // nur sprite anpassen
+            // Only adjust sprite
             ente_sprite.set_tiles(bn::sprite_items::ente.tiles_item().create_tiles(8));
         }
         
         if (moving) {
             action.update();
         } else {
-            // tiles nach bewegung setzen
-            if(direction == -1) {
+            // Set tiles after movement
+            if (direction == -1) {
                 ente_sprite.set_tiles(bn::sprite_items::ente.tiles_item(), 4);
             } else if (direction == 1) {
                 ente_sprite.set_tiles(bn::sprite_items::ente.tiles_item(), 0);
-            } else if (direction == 0) {
-                // nichts machen
             }
         }
-
-            
 
         bn::core::update();
     }
