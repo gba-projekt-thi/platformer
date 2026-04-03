021c3396d21894a07dc0b635a412a11ff12a9ed5
Tom <tof5038@thi.de>
2026-04-03 15:15:56 +0000

    refactor: pull request comments
    Added an enum to track last player input and update animation immediately
    
    Slowed down movement speed
    
    Used Code Formatter
    
    Refs: #12, #13

--- game/src/player.h
+++ game/src/player.h
@@ -6,25 +6,35 @@
 
 class Player
 {
-    public:
-        Player();
-        void update();
+public:
+    Player();
+    void update();
 
-    private:
-        // Configuration values
-        bn::sprite_ptr player_sprite;
-        int groundLevel;
-        bn::fixed acceleration;      
-        bn::fixed max_speed;
-        bn::fixed jump_speed;
-        bn::fixed gravity;
-        bn::fixed max_fall_speed;
+private:
+    // Used for sprite animation
+    enum class Facing
+    {
+        Forward,
+        Back,
+        Left,
+        Right
+    };
 
-        // Working values
-        bn::fixed x_coord;
-        bn::fixed y_coord;
-        bn::fixed x_velocity;
-        bn::fixed y_velocity;
-        bool canJump;
-        bn::sprite_animate_action<2> action;
+    // Configuration values
+    bn::sprite_ptr player_sprite;
+    int groundLevel;
+    bn::fixed acceleration;
+    bn::fixed max_speed;
+    bn::fixed jump_speed;
+    bn::fixed gravity;
+    bn::fixed max_fall_speed;
+
+    // Working values
+    bn::fixed x_coord;
+    bn::fixed y_coord;
+    bn::fixed x_velocity;
+    bn::fixed y_velocity;
+    bool canJump;
+    bn::sprite_animate_action<2> action;
+    Facing facing;
 };
