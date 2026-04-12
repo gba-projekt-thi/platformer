e3873614192a7950c3b0aa783c7f38f3782f0e55
chiaraO2O5 <chs5870@thi.de>
2026-04-12 08:18:46 +0000

    Added missing JSON file

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -6,6 +6,7 @@
 
 // Sprites
 #include "bn_sprite_items_ente.h"
+#include "bn_sprite_items_platformslevel1.h"
 
 // Wallpapers
 #include "bn_regular_bg_items_level1.h"
@@ -21,28 +22,26 @@ int main()
     // Prio 3 to be in the background
     bg.set_priority(3);
 
-    // Sprite wird erstellt
+    // Create character sprite
     bn::sprite_ptr ente_sprite = bn::sprite_items::ente.create_sprite(0, 0);
+    
+    // This is how to add sprites, just change the (0,0) to (16,0) etc.
+    bn::sprite_ptr platformlevel1_sprite = bn::sprite_items::platformslevel1.create_sprite(0, 0);
 
-    // hintergrund farbe
-    // grau     16,16,16
-    // blau     00,00,16
-    //      
-    // bn::bg_palettes::set_transparent_color(bn::color(12,20,31)); 
-    // für animation
+    // Animation
     bn::sprite_animate_action<2> action = bn::create_sprite_animate_action_forever(
                     ente_sprite, 26, bn::sprite_items::ente.tiles_item(), 0, 1);
-    // var für bewegungs-check
-    // 0 = nichts
-    // 1 = rechts
-    // -1 = links
+    // var for movement-check
+    // 0 = nothing
+    // 1 = right
+    // -1 = left
     int direction = 0;
     
     while(!bn::keypad::start_pressed())
     {
         bool moving = false;
 
-        // Bewegung
+        // Movement
         if(bn::keypad::left_held())
         {       
             ente_sprite.set_x(ente_sprite.x() - 1);
@@ -69,30 +68,28 @@ int main()
         if(bn::keypad::up_held())
         {
             direction = 0;
-            // nur sprite anpassen
+            // Change sprite only
             ente_sprite.set_tiles(bn::sprite_items::ente.tiles_item().create_tiles(9));
         }
         if(bn::keypad::down_held())
         {
             direction = 0;
-            // nur sprite anpassen
+            // Change sprite only
             ente_sprite.set_tiles(bn::sprite_items::ente.tiles_item().create_tiles(8));
         }
         
         if (moving) {
             action.update();
         } else {
-            // tiles nach bewegung setzen
+            // Set tiles after movement
             if(direction == -1) {
                 ente_sprite.set_tiles(bn::sprite_items::ente.tiles_item(), 4);
             } else if (direction == 1) {
                 ente_sprite.set_tiles(bn::sprite_items::ente.tiles_item(), 0);
             } else if (direction == 0) {
-                // nichts machen
+                // do nothing
             }
         }
-
-            
 
         bn::core::update();
     }
