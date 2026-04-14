b27d0e314231d4d98683edd27c8b197877ba2665
arnold2710 <arnold.streck@gmail.com>
2026-04-14 13:44:20 +0000

    feat(games/src): formatted code

--- game/src/death_counter.cpp
+++ game/src/death_counter.cpp
@@ -1,36 +1,31 @@
 #include "death_counter.h"
 
-
-void DeathCounter::on_player_death()
-{
+void DeathCounter::on_player_death() {
     ++_count;
 }
 
-unsigned int DeathCounter::count() const
-{
+auto DeathCounter::count() const -> unsigned int {
     return _count;
 }
 
-DeathCounterHUD::DeathCounterHUD(bn::sprite_text_generator& text_gen,
-                                 const DeathCounter& counter)
-    : _text_gen(text_gen), _counter(counter), _last_count(UINT32_MAX)
-{
+DeathCounterHUD::DeathCounterHUD(
+    bn::sprite_text_generator& text_gen,
+    const DeathCounter& counter)
+    : _text_gen(text_gen), _counter(counter), _last_count(UINT32_MAX) {
     refresh();
 }
 
-void DeathCounterHUD::update()
+void DeathCounterHUD::update()  // UI update
 {
-    if (_last_count != _counter.count())
-    {
+    if (_last_count != _counter.count()) {
         _last_count = _counter.count();
         refresh();
     }
 }
 
-void DeathCounterHUD::refresh()
-{
+void DeathCounterHUD::refresh() {
     _sprites.clear();
-    bn::string<16> text = "Deaths: ";
-    text += bn::to_string<8>(_counter.count());
+    bn::string<_stringlength> text = "Deaths: ";
+    text += bn::to_string<_countlength>(_counter.count());
     _text_gen.generate(-110, -70, text, _sprites);
 }
--- game/src/death_counter.h
+++ game/src/death_counter.h
@@ -1,31 +1,33 @@
 #pragma once
 
 #include <bn_sprite_text_generator.h>
-#include <bn_vector.h>
 #include <bn_string.h>
-#include "bn_sprite_ptr.h"  
+#include <bn_vector.h>
+#include "bn_sprite_ptr.h"
 
-class DeathCounter
-{
-private:
+class DeathCounter {
+   private:
     unsigned int _count = 0;
-public:
+
+   public:
     void on_player_death();
-    unsigned int count() const;   // simpelste Variante für GBA;
+    unsigned int count() const;
 };
 
-class DeathCounterHUD
-{
-public:
-    DeathCounterHUD(bn::sprite_text_generator& text_gen,
-                    const DeathCounter& counter);
+class DeathCounterHUD {
+   public:
+    DeathCounterHUD(
+        bn::sprite_text_generator& text_gen,
+        const DeathCounter& counter);
     void update();
 
-private:
+   private:
     void refresh();
+    static constexpr unsigned int _stringlength = 16;
+    static constexpr unsigned int _countlength = 8;
 
-    bn::sprite_text_generator&      _text_gen;
-    const DeathCounter&             _counter;
-    bn::vector<bn::sprite_ptr, 16>  _sprites;
-    uint32_t                        _last_count;
+    bn::sprite_text_generator& _text_gen;
+    const DeathCounter& _counter;
+    bn::vector<bn::sprite_ptr, 16> _sprites;
+    uint32_t _last_count;
 };
